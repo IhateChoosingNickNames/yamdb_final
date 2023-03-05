a18fc9186796c17c79709525bc5c8b92259fa736
Ларькин Михаил <jetpilot314@gmail.com>
2023-03-05 13:56:26 +0300

    Ver 1.3

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -19,20 +19,20 @@ class GenreAdmin(admin.ModelAdmin):
 
 @admin.register(Title)
 class TitleAdmin(admin.ModelAdmin):
-    list_display = ("pk", "name")
+    list_display = ("pk", "name", "year", "description", "category")
     search_fields = ("name",)
     empty_value_display = "-пусто-"
 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ("pk", "author")
+    list_display = ("pk", "author", "text", "pub_date", "score", "title")
     search_fields = ("author",)
     empty_value_display = "-пусто-"
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("pk", "author")
+    list_display = ("pk", "author", "text", "pub_date", "review")
     search_fields = ("author",)
     empty_value_display = "-пусто-"
